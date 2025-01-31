86c638dd348ab2d4b287673f23dd2113c73134b5
miguel-the <miguel.alonzo02@utrgv.edu>
2025-01-31 22:14:49 +0000

    : )

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Input
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter name:");
+        String Name = scanner.nextLine();  
+   
+        System.out.println("Enter age:");
+        int Age = scanner.nextInt();
+
+        System.out.println("Enter weight:");
+        int Weight = scanner.nextInt();
+
+        System.out.println("Do you smoke (y/n)?:");
+        String smokeInput = scanner.next();
+        Boolean Smoke = smokeInput.equals("y");
+
+        System.out.println("Name is: " + Name);
+        System.out.println("Age is: " + Age);
+        System.out.println("Weight is: " + Weight);
+        System.out.println("Smoker: " + Smoke);
+
+        scanner.close();
+    }
+}
